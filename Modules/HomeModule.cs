2e2fcc01b094347fd140fde4b3c3eb71ba9879e8
Adrian Agnic and Jordan Mysliwiec <adrian+jordan@>
2017-06-07 16:21:22 -0700

    start Nancy bones, start HTML

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,12 +1,16 @@
-// using System.Collections.Generic;
-// using Nancy;
-// using Nancy.ViewEngines.Razor;
-//
-// namespace FavoriteRestaurants
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get
-//     }
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace FavoriteRestaurants
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _=>
+      {
+        return View["index.cshtml"];
+      };
+      
+    }
