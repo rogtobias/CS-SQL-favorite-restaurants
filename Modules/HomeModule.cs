7ebe066a7f54cbb251abf8025d9917997ecff865
Adrian Agnic and Jordan Mysliwiec <adrian+jordan@>
2017-06-08 11:19:33 -0700

    add the views for new restaurants and cuisines, lists for restaurants and cuisines, and abilityies to get new restaurants and cuisines.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,5 +12,55 @@ namespace FavoriteRestaurants
       {
         return View["index.cshtml"];
       };
-      
+
+      Get["/restaurants"] = _ =>
+      {
+        List<Restaurant> allRestaurants = Restaurant.GetAll();
+        return View["restaurants.cshtml", allRestaurants];
+      };
+
+      Get["/cuisine"] = _ =>
+      {
+        List<Cuisine> allCuisine = Cuisine.GetAll();
+        return View["cuisine_types.cshtml", allCuisine];
+      };
+
+      Get["/cuisine/new"] = _ =>
+      {
+        return View["cuisine_form.cshtml"];
+      };
+
+      Post["/cuisine/new"] = _ =>
+      {
+        Cuisine newCuisine = new Cuisine(Request.Form["cuisine-type"]);
+        newCuisine.Save();
+        return View["success.cshtml"];
+      };
+
+      Get["/restaurants/new"] = _ =>
+      {
+        List<Cuisine> allCuisine = Cuisine.GetAll();
+        return View["restaurant_form.cshtml"];
+      };
+
+      Post["/restaurants/new"] = _ =>
+      {
+        Restaurant newRestaurant = new Restaurant(Request.Form["restaurant-name"],Request.Form["restaurant-alchohol"], Request.Form["cuisine-type"]);
+        newRestaurant.Save();
+        return View["success.cshtml"];
+      };
+      Post["/restaurants/delete"] = _ => {
+        Restaurant.DeleteAll();
+        return View["cleared.cshtml"];
+      };
+      Get["/cuisine/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Cuisine selectedCuisine = Cuisine.Find(parameters.id);
+        List<Restaurant> cuisineRestaurants = selectedCuisine.GetRestaurants();
+        model.Add("cuisine", selectedCuisine);
+        model.Add("restaurants", cuisineRestaurants);
+        return View["cuisine.cshtml", model];
+      };
     }
+  }
+}
